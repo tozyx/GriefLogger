074148994e5bfda310a0a1f5889e5d756b578eb9
冰焰 <95266625+tozyx@users.noreply.github.com>
2026-04-19 17:18:02 +0800

    Update InspectCommand to use dynamic permission

--- common/src/main/java/com/daqem/grieflogger/command/InspectCommand.java
+++ common/src/main/java/com/daqem/grieflogger/command/InspectCommand.java
@@ -6,14 +6,14 @@ import com.mojang.brigadier.builder.LiteralArgumentBuilder;
 import net.minecraft.commands.CommandSourceStack;
 import net.minecraft.commands.Commands;
 
-public class InspectCommand implements ICommand {
+公共 class InspectCommand implements ICommand {
 
 
     @Override
     public LiteralArgumentBuilder<CommandSourceStack> getCommand() {
         return Commands.literal("inspect")
-                .requires(source -> source.hasPermission(2))
-                .executes(context -> inspect(context.getSource()));
+                。requires(source -> source.hasPermission(GriefLoggerConfig.inspectPermission.get()))
+                。executes(context -> inspect(context.getSource()));
     }
 
     private static int inspect(CommandSourceStack source) {
